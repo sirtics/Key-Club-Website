1321af9eb0975c336b73cbca37c6aae4f7e7ef18
gimmy <gimmys943@gmail.com>
2024-05-10 19:55:59 -0400

    footer and header fully setup just gotta add actual stuff in each page

--- public/include.js
+++ public/include.js
@@ -1,17 +1,18 @@
-// adds on the header and footer html so i dont gotta do it on every page
 document.addEventListener("DOMContentLoaded", function () {
     const header = document.getElementById("header");
     const footer = document.getElementById("footer");
-
+  
+    // Include the header.html file
     fetch("header.html")
-        .then(response => response.text())
-        .then(html => {
-            header.innerHTML = html;
-        });
-
+      .then(response => response.text())
+      .then(html => {
+        header.innerHTML = html;
+      });
+  
+    // Include the footer.html file
     fetch("footer.html")
-        .then(response => response.text())
-        .then(html => {
-            footer.innerHTML = html;
-        });
-});
+      .then(response => response.text())
+      .then(html => {
+        footer.innerHTML = html;
+      });
+  });
